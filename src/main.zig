1f1cc36a573f107ec44e93cd5b89cc58f4060e8a
Knarkzel <knarkzel@gmail.com>
2024-10-12 11:03:43 +0200

    codegen from cli

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,26 @@
 const std = @import("std");
 const lexer = @import("lexer.zig");
-const parser = @import("lexer.zig");
+const parser = @import("parser.zig");
+const codegen = @import("codegen.zig");
 
 pub fn main() !void {
-    const file = @embedFile("examples/hello.chad");
-    std.debug.print("{s}\n", .{file});
+    // Allocator
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    // Get file
+    var arguments = std.process.args();
+    _ = arguments.skip();
+    const file = arguments.next() orelse return error.NoFileSpecified;
+
+    // Read and process file
+    const src = try std.fs.cwd().readFileAlloc(alloc, file, std.math.maxInt(usize));
+    const tokens = try lexer.lex(alloc, src);
+    const ast = try parser.parse(alloc, tokens.items);
+    const output = try codegen.codegen(alloc, ast.items);
+
+    // Print file
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{s}", .{output});
 }
