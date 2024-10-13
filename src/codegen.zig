082d207c2f82c39d7b675f58c2e955e8df16207f
Knarkzel <knarkzel@gmail.com>
2024-10-13 13:01:57 +0200

    move over to buffer, much better

--- src/codegen.zig
+++ src/codegen.zig
@@ -3,37 +3,46 @@ const lexer = @import("lexer.zig");
 const parser = @import("parser.zig");
 const assert = std.debug.assert;
 const ArrayList = std.ArrayList;
+const Buffer = std.io.BufferedWriter(4096, ArrayList(u8).Writer);
 
 pub fn codegen(alloc: std.mem.Allocator, ast: []const parser.Node) ![]const u8 {
     // Include header
-    var buffer = std.ArrayList(u8).init(alloc);
-    try buffer.appendSlice("const std = @import(\"std\");\n\n");
+    var output = std.ArrayList(u8).init(alloc);
+    var buffer = std.io.bufferedWriter(output.writer());
+    _ = try buffer.write("const std = @import(\"std\");\n\n");
 
     // Generate code
-    const file = try generate(alloc, ast);
-    try buffer.appendSlice(file);
-    return buffer.items;
+    var generator = Generator.init(alloc, ast, &buffer);
+    try generator.next();
+    try buffer.flush();
+    return output.items;
 }
 
-fn generate(alloc: std.mem.Allocator, ast: []const parser.Node) anyerror![]const u8 {
-    var generator = Generator.init(alloc, ast);
-    var buffer = std.ArrayList(u8).init(alloc);
-    while (try generator.next()) |code|
-        try buffer.appendSlice(code);
-    return buffer.items;
+// Helper function to generate sections of code
+fn generate(generator: *Generator, ast: []const parser.Node) !void {
+    var temp_generator = Generator.init(generator.alloc, ast, generator.buffer);
+    try temp_generator.next();
 }
 
 const Generator = struct {
+    // State
     alloc: std.mem.Allocator,
     ast: []const parser.Node,
+    buffer: *Buffer,
     index: usize,
-    indent: usize,
 
-    fn init(alloc: std.mem.Allocator, ast: []const parser.Node) Generator {
-        return Generator{ .alloc = alloc, .ast = ast, .index = 0, .indent = 0 };
+    fn init(alloc: std.mem.Allocator, ast: []const parser.Node, buffer: *Buffer) Generator {
+        return Generator{
+            .alloc = alloc,
+            .ast = ast,
+            .buffer = buffer,
+            .index = 0,
+        };
     }
 
-    fn next(self: *Generator) !?[]const u8 {
+    fn next(self: *Generator) anyerror!void {
+        // Get writer
+        var writer = self.buffer.writer();
         while (self.index < self.ast.len) {
             defer self.index += 1;
             const node = self.ast[self.index];
@@ -41,82 +50,53 @@ const Generator = struct {
             // Output Zig based on current node
             switch (node) {
                 // Value types
-                .boolean => |boolean| {
-                    const output = try std.fmt.allocPrint(self.alloc, "{}", .{boolean});
-                    return output;
-                },
-                .number => |number| {
-                    const output = try std.fmt.allocPrint(self.alloc, "{d}", .{number});
-                    return output;
-                },
-                .string => |string| return string,
+                .boolean => |boolean| try writer.print("{}", .{boolean}),
+                .number => |number| try writer.print("{d}", .{number}),
+                .string => |string| try writer.writeAll(string),
 
                 // Variables
                 .let => |let| {
                     // Write name and parameters
-                    var buffer = std.ArrayList(u8).init(self.alloc);
-                    try buffer.appendSlice("const ");
-                    try buffer.appendSlice(let.name);
-                    try buffer.appendSlice(" = ");
-                    const code = try generate(self.alloc, &.{let.value.*});
-                    try buffer.appendSlice(code);
-                    try buffer.appendSlice(";\n");
-                    return buffer.items;
+                    try writer.print("const {s} = ", .{let.name});
+                    try generate(self, &.{let.value.*});
+                    try writer.writeAll(";");
                 },
 
                 // Function call
                 .function_call => |function_call| {
-                    // Write name and parameters
-                    var buffer = std.ArrayList(u8).init(self.alloc);
-
                     // If name is special, translate it
                     if (std.mem.eql(u8, function_call.name, "print")) {
-                        try buffer.appendSlice("try std.io.getStdOut().writer().print(\"{any}\\n\", .{ ");
+                        try writer.writeAll("try std.io.getStdOut().writer().print(\"{any}\\n\", .{ ");
                     } else {
-                        try buffer.appendSlice(function_call.name);
-                        try buffer.append('(');
+                        try writer.print("{s}(", .{function_call.name});
                     }
-                    for (function_call.arguments) |argument| {
-                        const code = try generate(self.alloc, &.{argument});
-                        try buffer.appendSlice(code);
-                    }
+                    for (function_call.arguments) |argument|
+                        try generate(self, &.{argument});
                     if (std.mem.eql(u8, function_call.name, "print"))
-                        try buffer.appendSlice(" }");
-                    try buffer.appendSlice(");\n");
-                    return buffer.items;
+                        try writer.writeAll(" }");
+                    try writer.writeAll(");\n");
                 },
 
                 // Function declaration
                 .function_decl => |function_decl| {
                     // Write function declaration
-                    var buffer = std.ArrayList(u8).init(self.alloc);
                     if (std.mem.eql(u8, function_decl.name, "main"))
-                        try buffer.appendSlice("pub ");
-                    try buffer.appendSlice("fn ");
-                    try buffer.appendSlice(function_decl.name);
-                    try buffer.append('(');
-                    for (function_decl.parameters) |parameter| {
-                        const code = try generate(self.alloc, &.{parameter});
-                        try buffer.appendSlice(code);
-                    }
-                    try buffer.appendSlice(") !void {\n");
+                        try writer.writeAll("pub ");
+                    try writer.print("fn {s}(", .{function_decl.name});
+                    for (function_decl.parameters) |parameter|
+                        try generate(self, &.{parameter});
+                    try writer.writeAll(") !void {\n");
 
                     // Indend body
-                    self.indent += 4;
                     for (function_decl.body) |body| {
-                        const code = try generate(self.alloc, &.{body});
-                        if (self.indent > 0) {
-                            try buffer.appendSlice("    ");
-                        }
-                        try buffer.appendSlice(code);
+                        try writer.writeAll("    ");
+                        try generate(self, &.{body});
+                        try writer.writeAll("\n");
                     }
-                    self.indent -= 4;
-                    try buffer.appendSlice("}\n");
-                    return buffer.items;
+                    try writer.writeAll("}\n");
                 },
             }
         }
-        return null;
     }
 };
 
